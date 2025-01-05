67fd01e6f22d5b37942cfadea667e9d78f4966d8
Khaw Ming Sheng <xxxmingshengxxx@gmail.com>
2025-01-05 20:35:08 +0800

    update header img

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,16 +12,16 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/arona.jpg')}
+          style={styles.mainImg}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">BLUE ARCHIVE TOOL</ThemedText>
+        {/* <HelloWave /> */}
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">1: Future Banner List</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
@@ -36,13 +36,13 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText type="subtitle">2: Bond Exp Calculator</ThemedText>
         <ThemedText>
           Tap the Explore tab to learn more about what's included in this starter app.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        <ThemedText type="subtitle">3: Pyrox Income Calculator</ThemedText>
         <ThemedText>
           When you're ready, run{' '}
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
@@ -66,11 +66,8 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  mainImg: {
+   height: '100%',
+    width: 'auto'
   },
 });
--- app/ads/InlineAd.tsx
+++ app/ads/InlineAd.tsx
@@ -3,8 +3,8 @@ import * as Device from 'expo-device';
 import React, { useState } from 'react';
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
-const iosAdmobBanner = "ca-app-pub-12345678910/12345678910";
-const androidAdmobBanner = "ca-app-pub-12345678910/12345678910";
+const iosAdmobBanner = "ca-app-pub-7252717983754109/7446508117";
+const androidAdmobBanner = "ca-app-pub-7252717983754109/7446508117";
 const productionID = Device.osName === 'Android' ? androidAdmobBanner : iosAdmobBanner;
 
 const InlineAd = () => {
@@ -14,7 +14,7 @@ const InlineAd = () => {
       <BannerAd
         // It is extremely important to use test IDs as you can be banned/restricted by Google AdMob for inappropriately using real ad banners during testing
         unitId={__DEV__ ? TestIds.BANNER : productionID}
-        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        size={BannerAdSize.BANNER}
         requestOptions={{
           requestNonPersonalizedAdsOnly: true, 
           // You can change this setting depending on whether you want to use the permissions tracking we set up in the initializing
